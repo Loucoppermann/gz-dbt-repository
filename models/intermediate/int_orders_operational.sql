d8755c34b0ad73f83a126b44ccef2192ac06c444
coppermannlou22@gmail.com <coppermannlou22@gmail.com>
2023-10-31 15:30:31 +0000

    add operational_margin

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,10 @@
+select orders.orders_id, 
+    date_date,
+    round((margin+shipping_fee-logcost-cast(ship_cost as float64)),2) as operational_margin,
+    margin,
+    shipping_fee,
+    logcost,
+    cast(ship_cost as float64) as ship_cost
+from {{ref("int_orders_margin")}} as orders
+left join {{ref("stg_raw__ship")}} as ship
+on orders.orders_id=ship.orders_id
