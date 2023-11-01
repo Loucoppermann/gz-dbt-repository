d72eb51d40e6c0b8a87d2bac39adca2f02205144
coppermannlou22@gmail.com <coppermannlou22@gmail.com>
2023-11-01 15:16:32 +0000

    add macro function into int_sales_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,7 +6,7 @@ select date_date,
     products_id,
     ROUND(quantity*cast(purchase_price as float64), 2) AS purchase_cost,
     ROUND(revenue-quantity*cast(purchase_price as float64), 2) AS margin,
-    {{create_margin_percent(revenue, purchase_cost)}} as margin_percent
+    {{create_margin_percent('revenue', 'ROUND(quantity*cast(purchase_price as float64), 2)')}} as margin_percent
 from {{ref ('stg_raw__sales')}} as sales
 left join {{ref('stg_raw__product')}} as product
 on sales.pdt_id=product.products_id
