e4fd0a3f29acb9569efdf27fb120bbad792c5063
coppermannlou22@gmail.com <coppermannlou22@gmail.com>
2023-11-01 14:57:33 +0000

    function

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -5,7 +5,8 @@ select date_date,
     purchase_price,
     products_id,
     ROUND(quantity*cast(purchase_price as float64), 2) AS purchase_cost,
-    ROUND(revenue-quantity*cast(purchase_price as float64), 2) AS margin
+    ROUND(revenue-quantity*cast(purchase_price as float64), 2) AS margin,
+    {{create_margin_percent(revenue, purchase_cost)}} as margin_percent
 from {{ref ('stg_raw__sales')}} as sales
 left join {{ref('stg_raw__product')}} as product
 on sales.pdt_id=product.products_id
